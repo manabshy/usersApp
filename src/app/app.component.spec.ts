f1d0ebc67fa35793ff2c1451a0de022a2bd29c2b
MJ <manabshy@gmail.com>
2022-06-01 17:02:05 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -6,7 +7,8 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
@@ -30,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testApp app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Users');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpParams,HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testApp';
+  users: any
+  url: string;
+  email: string | undefined;
+
+  constructor(private httpClient: HttpClient) {
+    this.users = [];
+    this.url = 'https://jsonplaceholder.typicode.com/users'
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.httpClient.get(this.url).subscribe(users => {
+      console.log('users:', users)
+      this.users = users
+
+    });
+  }
+
+  get(id: number) {
+    console.log('get id');
+    this.httpClient.get(`https://jsonplaceholder.typicode.com/users/${id}`).subscribe((user:any) => {
+      if (user) {
+        this.email = user?.email;
+        console.log('user:', user);
+      }
+
+
+    });
+  }
 }
